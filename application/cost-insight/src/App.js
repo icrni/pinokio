d11ea7649b287e754d411647aa754783499f1d77
Igor Crnkovic <igor.crnkovic@t-matix.com>
2018-02-25 10:53:52 +0100

    reorganize app, add router

--- application/cost-insight/src/App.js
+++ application/cost-insight/src/App.js
@@ -1,80 +1,14 @@
-import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { ProjectCosts } from './ProjectCosts/ProjectCosts'
 
-import _ from 'lodash'
-import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar'
-import {CostTable} from './Costs'
 
-import ReactTable from "react-table";
-import "react-table/react-table.css";
+const App = () => (
+    <Router>
+        <div>
+            <Route exact path='/' component={ProjectCosts} />
+        </div>
+    </Router>
+)
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      labels: []
-    }
-  }
-
-  loadData() {
-    fetch('http://127.0.0.1:8000/roadmap/costs/')
-      .then(response => response.json())
-      .then(data => this.setState({ labels: data}));
-  }
-
-  componentDidMount() {
-    this.loadData();
-    //window.setInterval(this.loadData.bind(this), 60000);
-  }
-
-  render() {
-    return (
-      <MuiThemeProvider>
-      <div className="App">
-        <Toolbar>
-          <ToolbarGroup>
-            <ToolbarTitle text="Cost Insights" />
-          </ToolbarGroup>
-        </Toolbar>
-      </div>
-      <div>
-      <ReactTable
-        data={this.state.labels}
-        columns={[
-          {
-            Header: "Label",
-            accessor: "label"
-          },
-          {
-            Header: "Year",
-            accessor: "year"
-          },
-          {
-            Header: "Week",
-            accessor: "week"
-          },
-          {
-            Header: "PID",
-            accessor: "PID"
-          },
-          {
-            Header: "Cost (EUR)",
-            accessor: "cost",
-            aggregate: vals => _.sum(vals),
-            filterable: false
-          }
-        ]}
-      className="-striped -highlight"
-      pivotBy={["label", "year", "week", "PID"]}
-      filterable
-      />
-      </div>
-      </MuiThemeProvider>
-    );
-  }
-}
-
-export default App;
- 
+export default App
---
+++ application/cost-insight/src/ProjectCosts/ProjectCosts.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import './ProjectCosts.css';
+
+import _ from 'lodash'
+import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar'
+import {CostTable} from './Costs'
+
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
+class ProjectCosts extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      labels: []
+    }
+  }
+
+  loadData() {
+    fetch('http://127.0.0.1:8000/roadmap/costs/')
+      .then(response => response.json())
+      .then(data => this.setState({ labels: data}));
+  }
+
+  componentDidMount() {
+    this.loadData();
+    //window.setInterval(this.loadData.bind(this), 60000);
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider>
+      <div className="App">
+        <Toolbar>
+          <ToolbarGroup>
+            <ToolbarTitle text="Cost Insights" />
+          </ToolbarGroup>
+        </Toolbar>
+      </div>
+      <div>
+      <ReactTable
+        data={this.state.labels}
+        columns={[
+          {
+            Header: "Label",
+            accessor: "label"
+          },
+          {
+            Header: "Year",
+            accessor: "year"
+          },
+          {
+            Header: "Week",
+            accessor: "week"
+          },
+          {
+            Header: "PID",
+            accessor: "PID"
+          },
+          {
+            Header: "Cost (EUR)",
+            accessor: "cost",
+            aggregate: vals => _.sum(vals),
+            filterable: false
+          }
+        ]}
+      className="-striped -highlight"
+      pivotBy={["label", "year", "week", "PID"]}
+      filterable
+      />
+      </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export {ProjectCosts};
+ 
